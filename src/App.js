02ddda6bb65a142df2768a88f349fc1a3ccc08a4
oigomezz <36518231+oigomezz@users.noreply.github.com>
2018-03-24 18:47:11 -0500

    Componentes (Aun tengo algunos errores)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//Componets
+import Header from './components/Header.js';
+import Slider from './components/Slider.js';
+import Footer from './components/Footer.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,6 +18,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header />
+        <Slider />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return (
+			
+				<nav className="navbar navbar-default ">
+						<div className="container">
+								<div className="collapse navbar-collapse yamm" id="navigation">
+										<img src="assets/img/logo.png" width="270px" height="150px" alt=""/>
+										<div className="button navbar-right">
+											 <button className="navbar-btn nav-button wow bounceInRight login" onclick=" window.open('registro.html')" data-wow-delay="0.45s">Iniciar sesión</button>
+											 <button className="navbar-btn nav-button wow fadeInRight" onclick=" window.open('registro.html')" data-wow-delay="0.5s">Registro</button>
+										</div>
+										<ul className="main-nav nav navbar-nav navbar-right">
+									 
+												<li className="wow fadeInDown" data-wow-delay="0.1s"><a  data-delay="200" href="index.html">Inicio</a></li>
+												<li className="wow fadeInDown" data-wow-delay="0.2s"><a className="" href="index.html">Materias</a></li>
+												<li className="wow fadeInDown" data-wow-delay="0.3s"><a className="" href="#">Docentes</a></li>
+												<li className="wow fadeInDown" data-wow-delay="0.4s"><a href="">Recursos</a></li>
+												<li className="wow fadeInDown" data-wow-delay="0.5s"><a href="#">Contáctenos</a></li>
+										</ul>
+								</div>
+						</div>
+				</nav>
+			
+			
+		);
+	}
+}
+
+export default Header;
