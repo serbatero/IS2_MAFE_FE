2421165963e62e52e20b3677e2588419fe56f333
yaochoal <32651875+yaochoal@users.noreply.github.com>
2018-03-24 19:38:20 -0500

    avances
    menu y slider en react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Slider from './components/Slider';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Header/>
+      <Slider/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
