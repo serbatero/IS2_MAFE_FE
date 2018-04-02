2efc512a972d9667449749e527bb2cd525d3d304
yaochoal <32651875+yaochoal@users.noreply.github.com>
2018-04-01 19:31:10 -0500

    Oscar Implementacion
    Implementacion de oscar de Materias Docentes y Recursos con back y paginacion

---
+++ src/components/Docentes/Contenido.js
@@ -0,0 +1,24 @@
+// Dependencias
+import React, { Component } from 'react';
+import Docente from './Docente.js';
+
+class Contenido extends Component {
+	
+	render(){
+		
+		return(
+			<div className="col-md-9  pr0 padding-top-40 properties-page">
+				<div className="col-md-12 clear"> 
+					<div id="list-type" className="proerty-th">
+						{this.props.listado.map((docente)=>{
+								return <Docente key={docente.id} name={docente.name} scoreteacher_id={docente.scoreteacher_id} description={docente.description} />
+							})
+						}
+					</div>
+				</div>	
+			</div>							
+		);
+	}
+}
+
+export default Contenido;
--- src/components/Materias/index.js
+++ src/components/Materias/index.js
@@ -1,30 +1,70 @@
-// Dependencies
+// Dependencias
 import React, { Component } from 'react';
+
+//Componentes
 import Title from '../Global/Title';
+import Busqueda from '../Global//Busqueda.js';
+import Contenido from './Contenido.js';
+import baseURL from '../../url'
+
 class Materias extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { data_a: [] }
-    
+	constructor() {
+    super()
+    this.state = { data_a: [] , count: 1}
+     fetch(`${baseURL}/courses?page=${this.state.count}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+       this.setState({ data_a: data ,page :2})
+      })
+      this.handleCountClick = this.handleCountClick.bind(this);
   }
  
- componentWillMount() {
-    fetch('http://localhost:3000/courses')
+  handleCountClick(e) {
+    if (e.target.id === 'add') {
+      this.setState({
+        count: this.state.count + 1
+      });
+    } else if (e.target.id === 'subtract' && this.state.count > 1) {
+      this.setState({
+        count: this.state.count - 1
+      });
+    } else {
+      this.setState({
+        count: 1
+      });
+    }
+    fetch(`${baseURL}/courses?page=${this.state.count}`)
       .then((response) => {
         return response.json()
       })
       .then((data) => {
         this.setState({ data_a: data })
-        console.log(data)
       })
   }
-  render() {
-    return (
-     <div>
-     <Title title='Materias'/>
-     </div>
-    );
-  }
+
+	render(){
+		return(
+			<div>
+				<Title title="Materias"/>
+				<Busqueda />
+				<Contenido listado={this.state.data_a}/>
+				<div className="col-md-12"> 
+					<div className="pull-right">
+						<div className="pagination">
+								<ul>
+									<button type="submit" id = "subtract"onClick={this.handleCountClick} className="btn btn-default">Prev</button>
+									<button type="submit" className="btn btn-default">{this.state.count}</button>
+									<button type="submit" id = "add"onClick={this.handleCountClick} className="btn btn-default">Next</button>
+								</ul>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		);
+	}
 }
 
 export default Materias;
