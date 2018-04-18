3d6e3e41b194cbce1b879eeb522188821dc58f47
nabautistal <nabautistal@unal.edu.co>
2018-04-17 23:21:02 -0500

    validacion con back

--- src/components/Contacto/index.js
+++ src/components/Contacto/index.js
@@ -1,84 +1,11 @@
 // Dependencies
 import React, { Component } from 'react';
 import Title from '../Global/Title'
+import baseURL from '../../url'
 class Contacto extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			nombre : '',
-			nombreErr :'',
-			apellido : '',
-			apellidoErr: '',
-			correo : '',
-			correoErr : '',
-			asunto : '',
-			asuntoErr : '',
-			mensaje : '',
-			mensajeErr: ''
-
-		}
-        
-
-        this.handleInput =this.handleInput.bind(this);
-        this.validar = this.validar.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-
-	}
-
-    handleInput(e){
-
-
-        this.setState({
-            [e.target.name]:e.target.value
-        });
-
-        
-    };
-
-    validar(){
-
-        if (this.state.nombre.length <3 && this.state.nombre.length!==0){
-            this.setState({nombreErr: 'No es nombre valido'});
-        }
-
-        if(this.state.nombre.length >=3){
-            this.setState({nombreErr: ''});
-        }
-        if (this.state.apellido.length <3 && this.state.apellido.length!==0){
-            console.log(this.state.apellido.length);
-            this.setState({apellidoErr: 'No es apellido valido'});
-        }
-
-        if(this.state.apellido.length >=3){
-            this.setState({apellidoErr: ''});
-        }
-        if(this.state.correo.indexOf('@') === -1 && this.state.correo.length !==0){
-            this.setState({correoErr: 'No es una direccion de correo'});
-        }
-
-        if(this.state.correo.indexOf('@') > -1){
-            this.setState({correoErr: ''});
-        }
-
-
-
-        if (this.state.asunto.length <3 && this.state.asunto.length!==0){
-            this.setState({asuntoErr: 'No es asunto valido'});
-        }
-
-        if(this.state.asunto.length >=3){
-            this.setState({asuntoErr: ''});
-        }
-
-    }
-
-
-
-	onSubmit(e){
-
-		console.log(this.state);
-        e.preventDefault();
-        this.setState( {
+    constructor(props){
+        super(props);
+        this.state = {
             nombre : '',
             nombreErr :'',
             apellido : '',
@@ -90,48 +17,143 @@ class Contacto extends Component {
             mensaje : '',
             mensajeErr: ''
 
-        });
+        }
         
-		
-	};
+
+        this.handleInput =this.handleInput.bind(this);
+        this.validar = this.validar.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+
+    }
+
+    handleInput(e){
+
+
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+
+        
+    };
+
+    validar(e){
+
+        if ((this.state.nombre.length <3 && this.state.nombre.length!==0)|| ((this.state.nombre === "")&&(e.target.name ==='nombre'))){
+            this.setState({nombreErr: 'No es nombre valido'});
+        }
+
+        if(this.state.nombre.length >=3){
+            this.setState({nombreErr: ''});
+        }
+        if ((this.state.apellido.length <3 && this.state.apellido.length!==0) || ((this.state.apellido === "")&&(e.target.name ==='apellido'))){
+            console.log(this.state.apellido.length);
+            this.setState({apellidoErr: 'No es apellido valido'});
+        }
+
+        if(this.state.apellido.length >=3){
+            this.setState({apellidoErr: ''});
+        }
+        if((this.state.correo.indexOf('@') === -1 && this.state.correo.length !==0)||((this.state.correo === "")&&(e.target.name ==='correo'))){
+            this.setState({correoErr: 'No es una direccion de correo'});
+        }
+
+        if(this.state.correo.indexOf('@') > -1){
+            this.setState({correoErr: ''});
+        }
+
+
+
+        if ((this.state.asunto.length <3 && this.state.asunto.length!==0)||((this.state.asunto === "")&&(e.target.name ==='asunto'))){
+            this.setState({asuntoErr: 'No es asunto valido'});
+        }
+
+        if(this.state.asunto.length >=3){
+            this.setState({asuntoErr: ''});
+        }
+
+    }
+
+
+
+    onSubmit(e){
+
+        console.log(this.state);
+        e.preventDefault();
+        if((this.state.nombreErr !=="")||(this.state.apellidoErr !== "") ||(this.state.asuntoErr !== "") ||(this.state.correoErr !== "") ){
+           alert("Digite los campos señalados"); 
+        }else{
+            alert("Su mensaje ha sido enviado");
+        }
+        
+
+       /* this.setState( {
+            nombre : '',
+            nombreErr :'',
+            apellido : '',
+            apellidoErr: '',
+            correo : '',
+            correoErr : '',
+            asunto : '',
+            asuntoErr : '',
+            mensaje : '',
+            mensajeErr: ''
+
+        });*/
+
+      const contactParams = {"name": this.state.nombre, "lastname": this.state.apellido,
+      "email": this.state.correo, "subject": this.state.asunto, "message": this.state.mensaje}
+      const body = JSON.stringify(contactParams)
+      return fetch(`${baseURL}/contacts`, {
+      method: 'post',
+      body: body,
+      headers: {
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+      }
+      })
+      .then((res) => {
+        return res.json()
+      })   
+    };
 
 
   render() {
     return (
-       	<div>
-     	<Title title='Contáctenos'/>
+        <div>
+        <Title title='Contáctenos'/>
         <div className="col-md-8 col-md-offset-2"> 
-     	<h2>Formato de contacto</h2>
+        <h2>Formato de contacto</h2>
             <form>
                 <div className="row">
-                	<div className="col-sm-6">
-                		<div className="form-group">
-                			<label for="firstname" color='red'>Nombre(s) &nbsp; <font color='red'> {this.state.nombreErr}</font></label>
-                            <input type="text" className="form-control" id="firstname" name ='nombre'value={this.state.nombre} onChange = {this.handleInput}onBlur = {this.validar} required/>                        
+                    <div className="col-sm-6">
+                        <div className="form-group">
+                            <label  color='red'>Nombre(s) &nbsp; <font color='red'> {this.state.nombreErr}</font></label>
+                            <input type="text" className="form-control" id="firstname" name ='nombre'value={this.state.nombre} onChange = {this.handleInput}onInput = {this.validar} required/>                        
                         </div>
                     </div>
                     <div className="col-sm-6">
                         <div className="form-group">
-                            <label for="lastname">Apellido(s) &nbsp; <font color='red'> {this.state.apellidoErr}</font></label>
-                            <input type="text" className="form-control" id="lastname" name ='apellido' value={this.state.apellido} onChange = {this.handleInput} onBlur = {this.validar} required/>
+                            <label >Apellido(s) &nbsp; <font color='red'> {this.state.apellidoErr}</font></label>
+                            <input type="text" className="form-control" id="lastname" name ='apellido' value={this.state.apellido} onChange = {this.handleInput} onInput = {this.validar} required/>
                         </div>
                     </div>
                     <div className="col-sm-6">
                         <div className="form-group">
-                            <label for="email">Correo Electronico &nbsp; <font color='red'> {this.state.correoErr}</font></label>
-                            <input type="text" className="form-control" id="email" name='correo'value={this.state.correo} onChange = {this.handleInput} onBlur = {this.validar}required/>
+                            <label >Correo Electronico &nbsp; <font color='red'> {this.state.correoErr}</font></label>
+                            <input type="text" className="form-control" id="email" name='correo'value={this.state.correo} onChange = {this.handleInput} onInput = {this.validar}required/>
                         </div>
                     </div>
                     <div className="col-sm-6">
                         <div className="form-group">
-                            <label for="subject">Asunto &nbsp; {this.state.asuntoErr}</label>
-                            <input type="text" className="form-control" id="subject" name ='asunto'value={this.state.asunto} onChange = {this.handleInput } onBlur = {this.validar} required/>
+                            <label >Asunto &nbsp; <font color='red'>{this.state.asuntoErr}</font></label>
+                            <input type="text" className="form-control" id="subject" name ='asunto'value={this.state.asunto} onChange = {this.handleInput } onInput = {this.validar} required/>
 
                         </div>
                     </div>
                     <div className="col-sm-12">
                         <div className="form-group">
-                            <label for="message">Mensaje &nbsp; {this.state.mensajeErr}</label>
+                            <label >Mensaje &nbsp; <font>{this.state.mensajeErr}</font></label>
                             <textarea id="message" className="form-control" name='mensaje'value = {this.state.mensaje} onChange = {this.handleInput }></textarea>
                         </div>
                     </div>
@@ -143,8 +165,8 @@ class Contacto extends Component {
 
             </form>
 
-     	</div>
-     	</div>
+        </div>
+        </div>
     );
   }
 }
---
+++ src/components/Global/Busqueda.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class Busqueda extends Component {
+	
+	render(){
+		
+		return(
+			<div className="col-md-3 p0 padding-top-40">
+				<div className="blog-asside-right pr0">
+					<div className="panel panel-default sidebar-menu wow fadeInRight animated" >
+						<div className="panel-heading">
+							<h3 className="panel-title">Busqueda</h3>
+						</div>
+						<div className="panel-body search-widget">
+							<form action="" className=" form-inline"> 
+								<fieldset>
+									<div className="row">
+										<div className="col-xs-12">
+											<input type="text" className="form-control" placeholder="Buscar..."/>
+										</div>
+									</div>
+								</fieldset>
+
+								<fieldset>
+									<div className="row">
+										<div className="col-xs-6">
+											<select id="basic" className="selectpicker show-tick form-control">
+												<option> -Status- </option>
+												<option>Docente</option>
+												<option>Materia</option>
+												<option>Recurso</option>
+											</select>
+										</div>
+										<div className="col-xs-6">
+											<label >Score rango:</label>
+											<input type="text" className="span2" value="" data-slider-min="0" data-slider-max="5" data-slider-step="1" data-slider-value="[0,5]" id="price-range" /><br />
+											<b className="pull-left color">0</b>
+											<b className="pull-right color">500</b>
+										</div>
+									</div>
+								</fieldset>
+
+								
+								<fieldset >
+									<div className="row">
+										<div className="col-xs-12">  
+											<input className="button btn largesearch-btn" value="Buscar" type="submit" />
+										</div>  
+									</div>
+								</fieldset>
+							</form>
+						</div>
+					</div>
+
+			
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Busqueda;
