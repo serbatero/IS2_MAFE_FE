745033e09e16f0e401a5ffc83c8d41c528717544
Yesid Oc <yaochoal@unal.edu.co>
2018-04-27 23:19:09 -0500

    Implementación y coneccion de buscadores con el back

--- src/components/Global/Busqueda.js
+++ src/components/Global/Busqueda.js
@@ -1,7 +1,47 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import baseURL from '../../url'
 
 class Busqueda extends Component {
-	
+	constructor() {
+    super()
+    this.state = {  value: "teacher", texto: ""}
+    
+  }
+
+  cambiarEstado=(e)=>{
+  	
+      if(e.target.id==="basic"){
+      this.setState({value: e.target.value});
+      }
+
+      if(e.target.id==="texto"){
+        console.log(e.target.value)
+      this.setState({texto: e.target.value});
+      }
+  }
+  realizarBusqueda=(e)=>{
+   // console.log(this.state)
+    var ruta = "";
+    if(this.state.value==="teacher")
+      ruta = "docentes";
+    if(this.state.value==="resource")
+      ruta = "recursos";
+    if(this.state.value==="course")
+      ruta = "materias";
+     let axiosConfig = {headers: {'Content-Type': 'application/json;'}};
+        axios.post(`${baseURL}/search`, {
+         [`${this.state.value}_name`]: this.state.texto
+       }, axiosConfig)
+       .then(function (response) {
+        console.log(response.data[0].id)
+        window.location.replace(`${ruta}/${response.data[0].id}`);
+       //  this.setState({response.data.id});
+        })
+        .catch(function (error) {
+        console.log(error);
+       });
+  }
 	render(){
 		
 		return(
@@ -11,11 +51,11 @@ class Busqueda extends Component {
 							<h3 className="panel-title">Busqueda</h3>
 						</div>
 						<div className="panel-body search-widget">
-							<form action="" className=" form-inline"> 
+							<div action="" className=" form-inline"> 
 								<fieldset>
 									<div className="row">
 										<div className="col-xs-12">
-											<input type="text" className="form-control" placeholder="Buscar..."/>
+											<input type="text" id="texto" onChange={this.cambiarEstado} value={this.state.text} className="form-control" placeholder="Buscar..."/>
 										</div>
 									</div>
 								</fieldset>
@@ -23,12 +63,11 @@ class Busqueda extends Component {
 								<fieldset>
 									<div className="row">
 										<div className="col-xs-6">
-											<select id="basic" className="selectpicker show-tick form-control">
-												<option> -Status- </option>
-												<option>Docente</option>
-												<option>Materia</option>
-												<option>Recurso</option>
-											</select>
+										<select id="basic" className="selectpicker show-tick form-control" onChange={this.cambiarEstado} >
+                     				    <option value ="teacher">Docentes</option>
+                      					<option value ="course">Materias</option>
+                                        <option value ="resource">Recursos</option>
+                                        </select>
 										</div>
 										<div className="col-xs-6">
 											<label >Score rango:</label>
@@ -43,11 +82,11 @@ class Busqueda extends Component {
 								<fieldset >
 									<div className="row">
 										<div className="col-xs-12">  
-											<input className="button btn largesearch-btn" value="Buscar" type="submit" />
+											<input className="button btn largesearch-btn" onClick={this.realizarBusqueda} value="Buscar" type="submit" />
 										</div>  
 									</div>
 								</fieldset>
-							</form>
+							</div>
 						</div>
 					</div>
 
--- src/components/Inicio/index.js
+++ src/components/Inicio/index.js
@@ -1,10 +1,46 @@
 // Dependencies
 import React, { Component } from 'react';
-
-
+import axios from 'axios';
+import baseURL from '../../url'
 class Inicio extends Component {
- 
+  constructor() {
+    super()
+    this.state = {  value: "teacher", texto: ""}
+    
+  }
 
+  cambiarEstado=(e)=>{
+      if(e.target.id==="basic"){
+      this.setState({value: e.target.value});
+      }
+
+      if(e.target.id==="texto"){
+        console.log(e.target.value)
+      this.setState({texto: e.target.value});
+      }
+  }
+  realizarBusqueda=(e)=>{
+   // console.log(this.state)
+    var ruta = "";
+    if(this.state.value==="teacher")
+      ruta = "docentes";
+    if(this.state.value==="resource")
+      ruta = "recursos";
+    if(this.state.value==="course")
+      ruta = "materias";
+     let axiosConfig = {headers: {'Content-Type': 'application/json;'}};
+        axios.post(`${baseURL}/search`, {
+         [`${this.state.value}_name`]: this.state.texto
+       }, axiosConfig)
+       .then(function (response) {
+        console.log(response.data[0].id)
+        window.location.replace(`${ruta}/${response.data[0].id}`);
+       //  this.setState({response.data.id});
+        })
+        .catch(function (error) {
+        console.log(error);
+       });
+  }
   render() {
 
     return (
@@ -17,25 +53,31 @@ class Inicio extends Component {
             <div className="item"><img src="assets/img/slider-image-1.jpg" alt="imagen3" /></div>
           </div>
         </div>
+
         <div className="slider-content">
+
           <div className="row">
+
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+
               <h2>BUSCA INFORMACIÓN DE TUS DOCENTES, RECURSOS Y MATERIAS!</h2>
+
               <div className="search-form wow pulse" data-wow-delay="0.8s">
-                <form  className=" form-inline">
+
+                <div  className=" form-inline">
                   <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Buscar" />
+                    <input id="texto" type="text" className="form-control" onChange={this.cambiarEstado} value={this.state.text} placeholder="Buscar" />
                   </div>
                   <div className="form-group">                                     
-                    <select id="basic" className="selectpicker show-tick form-control">
-                      <option>Docentes</option>
-                      <option>Materias</option>
-                      <option>Recursos</option>
+                    <select id="basic" className="selectpicker show-tick form-control" onChange={this.cambiarEstado} >
+                      <option value ="teacher">Docentes</option>
+                      <option value ="course">Materias</option>
+                      <option value ="resource">Recursos</option>
                     </select>
                   </div>
-                  <button className="btn search-btn" type="submit"><i className="fa fa-search" /></button>
-                  <button className="btn search-btn prop-btm-sheaerch" type="submit"><i className="fa fa-search" /></button> 
-                </form>
+                  <button className="btn search-btn" onClick={this.realizarBusqueda}><i className="fa fa-search" /></button>
+                  <button className="btn search-btn prop-btm-sheaerch" ><i className="fa fa-search" /></button> 
+                </div>
               </div>
             </div>
           </div>
